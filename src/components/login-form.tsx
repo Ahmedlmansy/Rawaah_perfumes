ab874437395539def52a270130da897e63f13b85
Ahmed <ahmedelmansy579@gmail.com>
2025-11-23 18:18:05 +0200

    add some style

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -109,8 +109,8 @@ export function LoginForm({
           <Button type="submit" className="w-full" disabled={loading}>
             {loading ? "Logging in..." : "Login"}
           </Button>
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+          <div className="relative  text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-transparent px-2 text-muted-foreground">
               Or continue with
             </span>
           </div>
