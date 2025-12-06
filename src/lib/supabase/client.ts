02983d81233a842314e5275836f2732af90dbae8
Ahmed <ahmedelmansy579@gmail.com>
2025-12-06 20:52:46 +0200

    update client

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -6,3 +6,18 @@ export const createSupabaseClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 };
+
+
+
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+
+export function createSupabaseBrowser() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
